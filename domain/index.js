50401b8f10caadefc2ea7b6e631a2b4f0c83fe6f
brighthas <brighthas@brighthas.(none)>
2013-11-19 22:58:38 +0800

    add application layer.

---
+++ domain/index.js
@@ -0,0 +1,11 @@
+var domain = require("jsdm")();
+
+domain.register(
+    "AggreClass", require("./Column"), require("./Post")
+    // "listener", require("./eventHandles"),
+    // "repository", require("./repos"),
+    //  "commandHandle", require("./commandHandles"),
+    // "service",require("./services")
+)//.closeMethod()
+
+module.exports = domain;
