a964f6d1d71c0695f69a9b930dc2def00f61d531
brighthas <brighthas@brighthas.(none)>
2013-11-29 07:32:34 +0800

    add reply repository

--- domain/repos.js
+++ domain/repos.js
@@ -4,6 +4,37 @@ function wrap(my) {
 
     var replyRepo = new my.Repository("Reply");
 
+    replyRepo._create = function (args, callback) {
+        var Reply = my.Aggres.Reply;
+        var reply = new Reply(args);
+        callback(null, reply);
+    }
+
+    replyRepo._data2aggre = function (data) {
+
+        var Reply = my.Aggres.Reply;
+        var reply = new Reply(data);
+        reply._id = data.id;
+        reply._updateTime = data.updateTime;
+        reply._createTime = data.createTime;
+
+        return reply;
+    }
+
+    replyRepo._aggre2data = function (aggre) {
+        return {
+            id: aggre.id,
+            title: aggre._title,
+            body: aggre._body,
+            authorId: aggre._authorId,
+            parentId: aggre._parentId,
+            topicId: aggre._topicId,
+            updateTime: aggre._updateTime,
+            createTime: aggre._createTime
+        }
+    }
+
+
     var columnRepo = new my.Repository("Column");
     columnRepo._create = function (args, callback) {
         var Column = my.Aggres.Column;
@@ -23,8 +54,8 @@ function wrap(my) {
         }
     }
 
-    columnRepo._data2aggre = function(data){
-        var aggre = new my.Aggres.Column(data.name,data.des);
+    columnRepo._data2aggre = function (data) {
+        var aggre = new my.Aggres.Column(data.name, data.des);
         aggre._updateTime = data.updateTime;
         aggre._id = data.id;
         aggre._top = data.name;
