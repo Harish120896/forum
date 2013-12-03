2b830a79981700a8f8d83165a9c8974029313d63
brighthas <brighthas@gmail.com>
2013-12-03 19:39:03 +0800

    add command handle for Topic

--- domain/commandHandles.js
+++ domain/commandHandles.js
@@ -2,11 +2,36 @@ module.exports = wrap;
 
 function wrap(my){
 
-    handle1.commandName = "remove a post";
-
+    //////////////////////  command handle for Topic  ////////////////////
+    // create a topic.
+    handle1.commandName = "create a topic";
     function handle1(args,callback){
-
+        my.repos.Topic.create(args,callback);
     }
 
-    return [handle1]
+
+    // remove a topic
+    handle2.commandName = "remove a topic";
+    function handle2(args,callback){
+        my.repos.Topic.get(args.id,function(err,topic){
+            if(topic){
+                topic.removeReply();
+                topic.remove();
+            }
+            callback();
+        });
+    }
+
+    // update a topic
+    handle3.commandName = "update a topic";
+    function handle3(args,callback){
+        my.repos.Topic.get(args.id,function(err,topic){
+            if(topic){
+                topic.updateInfo(args.title,args.body,args.columnId);
+            }
+            callback();
+        })
+    }
+
+    return [handle1,handle2,handle3]
 }
