a230598376644b93402a0041adb8f743418559f7
brighthas <brighthas@gmail.com>
2014-01-25 23:16:49 +0800

    tabs to white

--- domain/Column.js
+++ domain/Column.js
@@ -1,66 +1,66 @@
 var uid = require("node-uuid").v1,
-	createModel = require("model-brighthas");
+    createModel = require("model-brighthas");
 
 
 module.exports = wrap;
 
 function wrap(my) {
 
-	var Column = createModel("Column")
-		.attr('id')
-		.attr("name", {
-			type: "string",
-			required: true
-		})
-		.attr("top_", {
-			type: "boolean",
-			default: false
-		})
-		.attr("accessNum", {
-			type: "number",
-		default:0
-		})
-		.attr("updateTime", {
-			type: "date"
-		})
-		.attr("createTime", {
-			type: "date"
-		})
-		.attr("des")
+    var Column = createModel("Column")
+        .attr('id')
+        .attr("name", {
+            type: "string",
+            required: true
+        })
+        .attr("top_", {
+            type: "boolean",
+            default: false
+        })
+        .attr("accessNum", {
+            type: "number",
+            default: 0
+        })
+        .attr("updateTime", {
+            type: "date"
+        })
+        .attr("createTime", {
+            type: "date"
+        })
+        .attr("des")
 
-	.on("creating", function(column) {
-		column.attrs.createTime = column.attrs.updateTime = new Date();
-	})
+    .on("creating", function(column) {
+        column.attrs.createTime = column.attrs.updateTime = new Date();
+    })
 
-	.on("changed", function(column, attrs) {
-		my.publish("*.*.update", "Column", column.id, this.toJSON(column, Object.keys(attrs)));
-	})
+    .on("changed", function(column, attrs) {
+        my.publish("*.*.update", "Column", column.id, this.toJSON(column, Object.keys(attrs)));
+    })
 
-	.method("up", function() {
-		this.updateTime = new Date;
-	})
+    .method("up", function() {
+        this.updateTime = new Date;
+    })
 
-	.method("top", function() {
-		this.top_ = true;
-	})
+    .method("top", function() {
+        this.top_ = true;
+    })
 
-	.method("untop", function() {
-		this.top_ = false;
-	})
+    .method("untop", function() {
+        this.top_ = false;
+    })
 
-	.method("access", function(readerId) {
-		this.accessNum = this.accessNum + 1;
-	})
+    .method("access", function(readerId) {
+        this.accessNum = this.accessNum + 1;
+    })
 
-	.method("updateInfo", function(name, des) {
-		this.set({
-			name: name,
-			des: des
-		});
-		return this.errors;
-	})
+    .method("updateInfo", function(name, des) {
+        this.set({
+            name: name,
+            des: des
+        });
+        return this.errors;
+    })
 
-	Column.className = "Column";
+    Column.className = "Column";
 
-	return Column;
+    return Column;
 }
