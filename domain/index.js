16b1af1d77426ca3fb5c8c0addbd174fcc495ff6
brighthas <brighthas@gmail.com>
2014-01-27 10:34:57 +0800

    update

--- domain/index.js
+++ domain/index.js
@@ -7,15 +7,15 @@ domain.register(
     require("./Topic"), 
 	require("./Reply"),
 	require("./Message"),
-    "get", function () {
-    },
+    "get", require("../app/db").get,
     "listener", require("./eventHandles"),require("../app/eventHandles"),
     "repository", require("./repos"),
     "commandHandle", require("./commandHandles"),
     "service", require("./services")(require("../app/query"))
 ).openMethod(
 	"User.plus",
-	"User.attr",
-	"User.updatePassword"
+	"User.updatePassword",
+	"User.follow",
+	"User.unfollow"
 )
 module.exports = domain;
