60f7421d90f102141c4d77a27274ac890bef9dcc
brighthas <brighthas@gmail.com>
2014-02-13 16:16:10 +0800

    restructuring

--- run.js
+++ run.js
@@ -1,19 +1,12 @@
 var express = require('express');
-
+var http = require('http');
 var userRoute = require('./route/user');
 var topicRoute = require("./route/topic");
 var columnRoute = require("./route/column");
 var replyRoute = require("./route/reply");
 
-var user = require('./routes/user');
-var http = require('http');
-// var validator = require("./validator");
-// var util = require("./util");
-// var data = require("./data");
-// var userCtrl = require("./controller/user");
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', require('ejs').__express);
@@ -27,7 +20,6 @@ app.use(express.cookieParser('your secret here'));
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 userRoute(app);
 topicRoute(app);
